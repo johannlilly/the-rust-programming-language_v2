45a9cf4f1714a587db12596a319eb54c6235dfa6
jlilly <johannlilly@users.noreply.github.com>
2018-05-03 12:29:19 +0200

    Parse input; shadowing

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,8 @@
 extern crate rand;
 
 use std::io;
-use rand::Rng; // in scope
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
@@ -15,5 +16,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
     	.expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse() // "shadow" the previous value of guess with a new one
+    	.expect("Please type a number!"); // Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+    	Ordering::Less => println!("Too small!"),
+    	Ordering::Greater => println!("Too big!"),
+    	Ordering::Equal => println!("You win!"),
+    }
 }
