329168878d7fa7a18c962a167a1b0438bacc33dc
jlilly <johannlilly@users.noreply.github.com>
2018-05-03 11:58:13 +0200

    Generate a random number with Rand crate
    "Knowing which traits to use and which functions and methods to call from a crate isn’t something that you’ll just know. Instructions for using a crate are in each crate’s documentation. Another neat feature of Cargo is that you can run the cargo doc --open command that will build documentation provided by all of your dependencies locally and open it in your browser. If you’re interested in other functionality in the rand crate, for example, run cargo doc --open and click rand in the sidebar on the left."

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,15 +1,19 @@
-// https://doc.rust-lang.org/book/second-edition/ch02-00-guessing-game-tutorial.html
-use std::io; // standard library
+extern crate rand;
 
-fn main() { // entry
-    println!("Guess the number!"); // ! macro
+use std::io;
+use rand::Rng; // in scope
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new(); // store user input. let is used to create variables. mut = mutable
-    //std::io::stdin
-    io::stdin().read_line(&mut guess) // read a mutable (mut) reference (&) of the line previously stored as mutable variable guess
-    	.expect("Failed to read line"); // error handling if this instance of io::Result is an Err value. By using expect not writing the error handling, the program crashes upon error. For this program, we want that.
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    	.expect("Failed to read line");
 
     println!("You guessed: {}", guess);
 }
