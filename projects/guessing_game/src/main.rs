47b561b46be4648d7753d6bb0c3b15f18ede18b2
jlilly <johannlilly@users.noreply.github.com>
2018-05-03 12:39:38 +0200

    To switch from expect to match, need to include match keyword

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -19,9 +19,9 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => continue, // The _ is a catchall value
         };
 
         println!("You guessed: {}", guess);
