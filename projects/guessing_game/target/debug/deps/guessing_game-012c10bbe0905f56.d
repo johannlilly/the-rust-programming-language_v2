2632af4ad6c7b6ac7f11eb9e9202b2e232a7d669
jlilly <johannlilly@users.noreply.github.com>
2018-05-03 11:50:45 +0200

    Using a Crate (library) to Get More Functionality

---
+++ projects/guessing_game/target/debug/deps/guessing_game-012c10bbe0905f56.d
@@ -0,0 +1,5 @@
+/Users/user/Documents/GitHub/the-rust-programming-language_v2/projects/guessing_game/target/debug/deps/guessing_game-012c10bbe0905f56: src/main.rs
+
+/Users/user/Documents/GitHub/the-rust-programming-language_v2/projects/guessing_game/target/debug/deps/guessing_game-012c10bbe0905f56.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/deps/rand-6f4fb63a43b18fa7.d
@@ -0,0 +1,7 @@
+/Users/user/Documents/GitHub/the-rust-programming-language_v2/projects/guessing_game/target/debug/deps/librand-6f4fb63a43b18fa7.rlib: /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs
+
+/Users/user/Documents/GitHub/the-rust-programming-language_v2/projects/guessing_game/target/debug/deps/rand-6f4fb63a43b18fa7.d: /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs /Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs
+
+/Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs:
+/Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs:
+/Users/user/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs:
